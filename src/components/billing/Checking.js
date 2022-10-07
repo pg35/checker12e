22476dc6f8b53c4740e58220f4106ee1d927d1dc
pg35 <pg35@users.noreply.github.com>
2022-10-08 02:16:33 +0500

    Initial commit

---
+++ src/components/billing/Checking.js
@@ -0,0 +1,37 @@
+import Request from "../util/Request";
+import * as T from "../../reducer/action";
+
+export default function Checking(props) {
+  const { state, dispatch, repeatCount = 100 } = props;
+  const { scan } = state;
+  return (
+    <Request
+      {...props}
+      useAjaxArgs={{
+        ajax: {
+          data: {
+            action: "pxq_pgck_get_check_credits_result",
+            scan_id: scan.id
+          }
+        },
+        repeatCount: repeatCount
+      }}
+      repeatCount={repeatCount}
+      onComplete={(data, stopAjax) => {
+        if (!data.credits) return;
+        dispatch(
+          T.createAction(T.SCAN, {
+            status: "checked",
+            cost: data.credits
+          })
+        );
+        stopAjax();
+      }}
+      messages={[
+        "Calculating cost",
+        "Cost calculated",
+        "Cost calculation timed out"
+      ]}
+    />
+  );
+}
